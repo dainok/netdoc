a2357dde4b9294f79afb68c8c2cf09cb98d5fefd
Andrea Dainese <andrea.dainese@pm.me>
2023-06-12 15:29:25 +0200

    Linting

--- netdoc/search.py
+++ netdoc/search.py
@@ -9,12 +9,13 @@ __copyright__ = "Copyright 2022, Andrea Dainese"
 __license__ = "GPLv3"
 
 from netbox.search import SearchIndex, register_search
-from .models import ArpTableEntry, Discoverable, MacAddressTableEntry, RouteTableEntry
+from .models import ArpTableEntry, MacAddressTableEntry, RouteTableEntry
 
 
 @register_search
 class ArpTableEntryIndex(SearchIndex):
     """Includes ArpTableEntry in global search."""
+
     model = ArpTableEntry
     fields = [
         ["ip_address", 100],
@@ -26,15 +27,18 @@ class ArpTableEntryIndex(SearchIndex):
 @register_search
 class MacAddressTableEntryIndex(SearchIndex):
     """Includes MacAddressTableEntry in global search."""
+
     model = MacAddressTableEntry
     fields = [
         ["mac_address", 100],
         ["vendor", 100],
     ]
 
+
 @register_search
 class RouteTableEntryIndex(SearchIndex):
     """Includes RouteTableEntry in global search."""
+
     model = RouteTableEntry
     fields = [
         ["destination", 100],
