49d6a4b3e2c1fc2c31461bd67a5bc5292be174ed
Andrea Dainese <andrea.dainese@pm.me>
2023-06-15 15:13:10 +0200

    Fix open redirect

--- netdoc/views.py
+++ netdoc/views.py
@@ -391,9 +391,9 @@ class DiscoverableDiscoverView(generic.ObjectDeleteView):
             }
             utils.spawn_script("Discover", user=request.user, post_data=data)
 
-            return_url = form.cleaned_data.get("return_url")
-            if return_url and return_url.startswith("/"):
-                return redirect(return_url)
+            # return_url = form.cleaned_data.get("return_url")
+            # if return_url and return_url.startswith("/"):
+            #     return redirect(return_url)
             return redirect(self.get_return_url(request, obj))
 
         logger.debug("Form validation failed")
