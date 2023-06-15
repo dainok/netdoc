d672716ad3ce199530ec9fc95338d76dfc808869
Andrea Dainese <andrea.dainese@pm.me>
2023-06-15 10:56:19 +0200

    Fixes inventory scripts

--- netdoc/ingestors/netmiko_cisco_xr_admin_show_inventory.py
+++ netdoc/ingestors/netmiko_cisco_xr_admin_show_inventory.py
@@ -6,23 +6,33 @@ __license__ = "GPLv3"
 
 from netdoc.schemas import device
 
+# See: https://github.com/netbox-community/devicetype-library
+MANUFACTURER = "Cisco"
+DEFAULT_MODEL = "Cisco Unknown device"
+
 
 def ingest(log):
     """Processing parsed output."""
     device_o = log.discoverable.device
+    part_number = DEFAULT_MODEL
+    part_serial_number = None
 
     for item in log.parsed_output:
         # See https://github.com/networktocode/ntc-templates/tree/master/tests/cisco_xr/admin_show_inventory # pylint: disable=line-too-long
         part_description = item.get("name")
-        part_serial_number = item.get("sn")
-        part_number = item.get("pid")
 
         if "chassis" in part_description.lower():
             # Chassis model and Serial Number
-            device.update(
-                device_o, serial=part_serial_number, model_keyword=part_number
-            )
+            part_serial_number = item.get("sn")
+            part_number = item.get("pid")
             break
+
+    device.update(
+        device_o,
+        serial=part_serial_number,
+        manufacturer=MANUFACTURER,
+        model_keyword=part_number,
+    )
 
     # Update the log
     log.ingested = True
