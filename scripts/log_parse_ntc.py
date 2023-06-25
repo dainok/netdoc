707e7aafcd0b624490c422344cd222c0e8000aaa
Andrea Dainese <andrea.dainese@pm.me>
2023-06-25 09:43:28 +0200

    Update scripts

--- scripts/log_parse_ntc.py
+++ scripts/log_parse_ntc.py
@@ -8,7 +8,9 @@ from netmiko.utilities import get_structured_data
 
 from netdoc.models import DiscoveryLog
 
-LOG_ID = 3345  # DiscoveryLog ID
+LOG_ID = 661  # DiscoveryLog ID
+
+# Don't edit below this line
 
 log = DiscoveryLog.objects.get(id=LOG_ID)
 platform = "_".join(  # pylint: disable=invalid-name
