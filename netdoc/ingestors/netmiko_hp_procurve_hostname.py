c4f21434b54c2b85d7e8db8e181c7f858633d4a1
Your Name <user@example.com@example.com>
2023-07-21 15:49:43 +0000

    update

--- netdoc/ingestors/netmiko_hp_procurve_hostname.py
+++ netdoc/ingestors/netmiko_hp_procurve_hostname.py
@@ -4,11 +4,7 @@ __contact__ = "andrea@adainese.it"
 __copyright__ = "Copyright 2022, Andrea Dainese"
 __license__ = "GPLv3"
 
-<<<<<<< HEAD
 import re
-=======
-from netmiko.utilities import get_structured_data
->>>>>>> 65cb3d6a9c7166c52089c9d4d3f4314a1fc4e7d2
 
 from netdoc.schemas import device, discoverable
 from netdoc import utils
@@ -24,14 +20,7 @@ def ingest(log):
     try:
         name = re.findall(r".*System Name\s*:\s*(\S+).*$", name, re.MULTILINE | re.DOTALL).pop()
     except AttributeError as exc:
-<<<<<<< HEAD
         raise AttributeError(f"Failed to match HOSTNAME regex on {name}") from exc
-=======
-        raise AttributeError("Failed to decode HOSTNAME") from exc
-    if not name:
-        raise AttributeError("Failed to decode HOSTNAME")
-
->>>>>>> 65cb3d6a9c7166c52089c9d4d3f4314a1fc4e7d2
     name = utils.normalize_hostname(name)
 
     # Get or create Device
