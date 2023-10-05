eae87fb16d041a18a0a70d393a8d5c943709edf2
Andrea Dainese <andrea.dainese@pm.me>
2023-10-05 17:57:17 +0200

    remove comment

--- netdoc/utils.py
+++ netdoc/utils.py
@@ -747,7 +747,7 @@ def normalize_interface_speed(speed):
         # Speed is set to auto
         return None
     speed = speed.replace(" ", "")
-    speed = speed.replace(".", "")  # HP Procurve
+    speed = speed.replace(".", "")
     speed = speed.replace("fdx", "")  # HP Procurve
     speed = speed.replace("hdx", "")  # HP Procurve
     speed = speed.replace("kbit", "")
