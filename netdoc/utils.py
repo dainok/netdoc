0acc8e4c540afdd4bb60e3c2a3d3ca982e2281c6
Andrea Dainese <andrea.dainese@pm.me>
2023-10-20 18:20:39 +0200

    Update utils.py

--- netdoc/utils.py
+++ netdoc/utils.py
@@ -763,7 +763,9 @@ def normalize_interface_speed(speed):
         return None
     speed = speed.replace(" ", "")
     speed = speed.replace(".", "")
+    speed = speed.replace("fd", "")  # HP Procurve
     speed = speed.replace("fdx", "")  # HP Procurve
+    speed = speed.replace("hd", "")  # HP Procurve
     speed = speed.replace("hdx", "")  # HP Procurve
     speed = speed.replace("kbit", "")
     speed = speed.replace("kbps", "")
@@ -771,7 +773,7 @@ def normalize_interface_speed(speed):
     speed = speed.replace("mb/s", "000")
     speed = speed.replace("gb/s", "000000")
     speed = speed.replace("gbps", "000000")
-    speed = speed.replace("gigfd", "000") # HP Procurve
+    speed = speed.replace("gig", "000000")  # HP Procurve
     try:
         speed = int(speed)
     except ValueError as exc:
