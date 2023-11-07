3a3b2bb4b572e9312c0507f5a4394c9dcd8d9aab
Andrea Dainese <andrea.dainese@pm.me>
2023-11-07 18:29:49 +0100

    add tests

--- scripts/lab_load.py
+++ scripts/lab_load.py
@@ -1,6 +1,6 @@
 """Blank the current database and load a specific scenario."""
 from netdoc.tests.test import load_scenario
 
-LAB_DIR = "netdoc/tests/netmiko_cisco_nxos/lab1"
+LAB_DIR = "netdoc/tests/netmiko_aruba_oscx/lab1"
 
 load_scenario(LAB_DIR)
